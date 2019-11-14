dcbafedd6da7e158bacc88d45f80b16bab9a9f08
Florian-ITESCIA <57197559+Florian-ITESCIA@users.noreply.github.com>
2019-11-14 09:47:08 +0100

    Update Exo 18.py

--- Exo 18.py
+++ Exo 18.py
@@ -7,7 +7,7 @@ def enquete(name) :
         adn = "AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGTACTCCGCGCGCCGGGACAGAATGCC"
     if name == 'M. Leblanc':
         adn = "CTGCAGGAACTTCTTCTGGAAGTACTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAG"
-    if adn.find("CATA") >= 0 and adn.find("ATGC") >= 0 and adn.find("CATATGC") == -1 and adn.find("ATGCCATA") == -1:
+    if adn.find("CATA") >= 0 and adn.find("ATGC") >= 0 and adn.find("CATATGC") == -1 and adn.find("ATGCCATA") == -1: #on vérifie si le find trouve la chaine, il renvoie la position (donc >= 0), il faut également que find ne trouve pas les 2 chaines collées (donc == -1)
         print(name + " est le coupable")
     else :
         print(name + ' est innocent')
